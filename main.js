7d0a136477fd83c3ea1eaf71c3a33445ec748c1e
Lexx2609 <2433053@mail.ru>
2023-07-09 17:30:17 +0500

    lesson10

--- main.js
+++ main.js
@@ -1,41 +1,48 @@
-const mass = ['one','two','three','four','five','six','seven','eight','nine','ten']
-const mass2 = [
-    ['one','two'],
-    ['three','four'],
-    ['five','six'],
-    ['seven','eight'],
-    ['nine','ten']
-]
-let a = document.querySelector('.two')
-let b = document.querySelector('#one')
-a.onclick = function () {
-    let c = mass[4]
-    b.innerHTML = `<h1>${c}</h1>`
-}
-const num = [1,2,3,4,5,6,7,8,9]
-let e = document.querySelector('.add')
-e.onclick = function () {
-    let even = num.filter(function (element) {
-        return element % 2 === 0
-    })
-    b.innerHTML = `<h1>${even}</h1>`
-}
-let d = document.querySelector('.del')
-d.onclick = function () {
-    let odd = num.filter(function (element) {
-        return element % 2 !== 0
-    })
-    b.innerHTML = `<h1>${odd}</h1>`
-}
-let m = document.querySelector('.four')
-m.onclick = function () {
-    for (let i = 0; mass2.length > i; i++) {
-        let c = mass2[i];
-        for (let k = 0; k < c.length; k++) {
-            console.log(c[k]);
-            b.innerHTML = `<h1>${c[k]}</h1>`
-        }
-    }
-}
+let first = document.querySelector(".first");
+let last = document.querySelector(".last");
+let title = document.querySelector(".title");
 
-
+fetch("https://randomuser.me/api")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        first.innerHTML += `<img src =${data.results[0].picture.large} />`
+        first.innerHTML += `<h2>Имя: ${data.results[0].name.first}</h2>`
+        first.innerHTML += `<h2>Фамилия : ${data.results[0].name.last}</h2>`
+        first.innerHTML += `<h2>Телефон : ${data.results[0].phone}</h2>`
+        first.innerHTML += `<h2>возраст: ${data.results[0].dob.age}</h2>`
+        first.innerHTML += `<h2>Почта: ${data.results[0].email}</h2>`
+        first.innerHTML += `<h2>Пол: ${data.results[0].gender}</h2>`
+        first.innerHTML += `<h2>ID: ${data.results[0].id.value}</h2>`
+        console.log(data)
+    });
+fetch("https://randomuser.me/api")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        last.innerHTML += `<img src =${data.results[0].picture.large} />`
+        last.innerHTML += `<h2>Имя: ${data.results[0].name.first}</h2>`
+        last.innerHTML += `<h2>Фамилия : ${data.results[0].name.last}</h2>`
+        last.innerHTML += `<h2>Телефон : ${data.results[0].phone}</h2>`
+        last.innerHTML += `<h2>возраст: ${data.results[0].dob.age}</h2>`
+        last.innerHTML += `<h2>Почта: ${data.results[0].email}</h2>`
+        last.innerHTML += `<h2>Пол: ${data.results[0].gender}</h2>`
+        last.innerHTML += `<h2>ID: ${data.results[0].id.value}</h2>`
+        console.log(data)
+    });fetch("https://randomuser.me/api")
+    .then(function (resp) {
+        return resp.json();
+    })
+    .then(function (data) {
+        title.innerHTML += `<img src =${data.results[0].picture.large} />`
+        title.innerHTML += `<h2>Имя: ${data.results[0].name.first}</h2>`
+        title.innerHTML += `<h2>Фамилия : ${data.results[0].name.last}</h2>`
+        title.innerHTML += `<h2>Телефон : ${data.results[0].phone}</h2>`
+        title.innerHTML += `<h2>возраст: ${data.results[0].dob.age}</h2>`
+        title.innerHTML += `<h2>Почта: ${data.results[0].email}</h2>`
+        title.innerHTML += `<h2>Пол: ${data.results[0].gender}</h2>`
+        title.innerHTML += `<h2>ID: ${data.results[0].id.value}</h2>`
+        console.log(data)
+    });
